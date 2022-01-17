b3ac62c6cb097097b08c8e12921a895b4fbcbe82
muradumudov115 <murad@DESKTOP-TQKOADC>
2022-01-17 13:03:54 +0400

    Homework

---
+++ MVC.task/DataTransfer/DataTransfer/Properties/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTransfer.Properties.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //var view = new ViewResult();
+            //view.ViewName = "Index";
+            //return view();
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult GetFile()
+        {
+            return File("~/img/sekil4.jpg", "image.jpg");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+        
+
+
+    }
+}
